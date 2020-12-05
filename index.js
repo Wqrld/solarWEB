dcf5437594fa1bc121329a4f493569622bd48753
Your Name <you@example.com>
2020-12-05 04:33:22 -0800

    config changes & page template

--- index.js
+++ index.js
@@ -1,20 +1,48 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+var mysql = require("mysql");
+var config = require("config.json");
+
+var connection = mysql.createPool({
+    connectionLimit: 4, // Faster connections.
+    host: config.db_host,
+    user: config.db_user,
+    password: config.db_password,
+    database: config.db_name,
+    multipleStatements: true
+});
 
 const app = express();
 
 app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
+    bodyParser.urlencoded({
+        extended: true
+    })
 );
 app.use("/static", express.static("static"));
 
 app.use(bodyParser.json());
 
-app.get("/", function(req, res) {
-res.render("index.ejs", {})
+app.get("/", function (req, res) {
+    db.query("SELECT * FROM readings", [], function (err, response) {
+        res.render("index.ejs", { readings: response })
+    })
 });
+
+app.post("/newdata", function (req, res) {
+    if (req.body.apikey == config.apikey) {
+
+
+        db.query("INSERT into readings set ?", { wattAC: req.body.wattAC }, function (err) {
+
+            res.send("success")
+
+        })
+
+    } else {
+        res.send("not allowed", 403)
+    }
+})
 
 app.enable("trust proxy");
 app.set("view engine", "ejs");
@@ -23,4 +51,4 @@ app.set("views", "views");
 
 
 console.log("loaded");
-app.listen("8608");
+app.listen(config.port);
